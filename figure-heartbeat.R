2812c0b2db9cbe7101b6e4c9282f637d299d6475
Toby Dylan Hocking <tdhock5@gmail.com>
2017-05-29 11:34:55 -0400

    unconstrained figures

--- figure-heartbeat.R
+++ figure-heartbeat.R
@@ -1,6 +1,7 @@
 works_with_R(
   "3.3.3",
   seewave="2.0.5", tuneR="1.3.2",
+  Segmentor3IsBack="2.0",
   "tdhock/coseg@49d056963d8c0eaebea22fef453842255a330a05",
   ggplot2="2.1.0")
 
@@ -38,6 +39,124 @@ end <- count.dt[peaks$chromEnd, seconds]
 peaks[, start.seconds := start]
 peaks[, end.seconds := end]
 
+max.segments <- max.peaks*2+1
+fit.segmentor <- Segmentor(some.int, Kmax=max.segments)
+
+seg.mean.vec <- fit.segmentor@parameters[max.segments,]
+seg.end.vec <- fit.segmentor@breaks[max.segments,]
+seg.start.vec <- c(1L, seg.end.vec[-max.segments]+1)
+data.mean.vec <- rep(seg.mean.vec, seg.end.vec-seg.start.vec+1)
+segmentor.seg.dt <- data.table(
+  start=seg.start.vec,
+  end=seg.end.vec,
+  mean=seg.mean.vec,
+  start.seconds=(seg.start.vec-1)*sec.diff,
+  end.seconds=seg.end.vec*sec.diff)
+segmentor.break.dt <- data.table(
+  pos=seg.start.vec[-1],
+  seconds=segmentor.seg.dt$start.seconds[-1],
+  diff=diff(seg.mean.vec))
+segmentor.break.dt[, sign := sign(diff)]
+segmentor.break.dt[, feasible.sign := c(1, -1)]
+segmentor.break.dt[, status := ifelse(
+  sign==feasible.sign, "feasible", "infeasible")]
+
+gg <- ggplot()+
+  theme_bw()+
+  geom_line(aes(
+    seconds, count),
+    data=count.dt)+
+  geom_segment(aes(
+    start.seconds, mean,
+    xend=end.seconds, yend=mean),
+    data=segmentor.seg.dt,
+    color="green",
+    size=2)+
+  geom_vline(aes(
+    linetype=status,
+    xintercept=seconds),
+    data=segmentor.break.dt,
+    color="green")+
+  ylab("Amplitude (higher for louder heartbeat)")
+print(gg)
+
+segmentor.loss.dt <- data.table(
+  model="unconstrained",
+  total.PoissonLoss=PoissonLoss(some.int, data.mean.vec))
+segmentor.loss.dt[, mean.PoissonLoss := total.PoissonLoss/length(data.mean.vec)]
+constrained.loss.dt <- data.table(
+  model="PeakSeg",
+  total.PoissonLoss=subset(fit$loss, peaks==max.peaks)$PoissonLoss)
+constrained.loss.dt[, mean.PoissonLoss := total.PoissonLoss/length(data.mean.vec)]
+rbind(segmentor.loss.dt, constrained.loss.dt)
+
+gg.unconstrained <- ggplot()+
+  theme_bw()+
+  geom_vline(aes(
+    color=model,
+    linetype=status,
+    xintercept=seconds),
+    data=data.table(segmentor.break.dt, model="unconstrained"))+
+  geom_line(aes(
+    seconds, count),
+    data=count.dt)+
+  geom_segment(aes(
+    start.seconds, mean,
+    color=model,
+    xend=end.seconds, yend=mean),
+    data=data.table(segmentor.seg.dt, model="unconstrained"),
+    size=2)+
+  geom_label(aes(
+    6, 18000, label=sprintf("unconstrained PoissonLoss=%.0f", mean.PoissonLoss),
+    color=model),
+    hjust=1,
+    data=segmentor.loss.dt)+
+  ylab("Amplitude (higher for louder heartbeat)")+
+  scale_color_manual(values=c(
+    PeakSeg="#E41A1C",
+    "deepskyblue",
+    unconstrained="#377EB8",
+    "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", 
+    "#A65628", "#F781BF", "#999999"))+
+  guides(color="none")
+print(gg.unconstrained)
+pdf("figure-heartbeat-unconstrained.pdf", 10)
+print(gg.unconstrained)
+dev.off()
+
+constrained.segs <- data.table(fit$segments)[segments==max.segments]
+constrained.segs[, start.seconds := chromStart*sec.diff]
+constrained.segs[, end.seconds := chromEnd*sec.diff]
+constrained.break.dt <- constrained.segs[, list(
+  model="PeakSeg",
+  pos=chromStart[-1],
+  seconds=start.seconds[-1],
+  diff=diff(mean))]
+constrained.break.dt[, sign := sign(diff)]
+constrained.break.dt[, feasible.sign := c(1, -1)]
+constrained.break.dt[, status := ifelse(
+  sign==feasible.sign, "feasible", "infeasible")]
+gg.PeakSeg <- gg.unconstrained+
+  geom_segment(aes(
+    seconds, 15000,
+    color=model,
+    linetype=status,
+    xend=seconds, yend=0),
+    data=constrained.break.dt)+
+  geom_segment(aes(
+    start.seconds, mean,
+    color=model,
+    xend=end.seconds, yend=mean),
+    data=data.table(constrained.segs, model="PeakSeg"),
+    size=1)+
+  geom_label(aes(
+    6, 20000, label=sprintf("PeakSeg PoissonLoss=%.0f", mean.PoissonLoss),
+    color=model),
+    hjust=1,
+    data=constrained.loss.dt)
+pdf("figure-heartbeat-PeakSeg.pdf", 10)
+print(gg.PeakSeg)
+dev.off()
 
 bpm <- data.table(
   beats=max.peaks,
